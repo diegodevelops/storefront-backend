5a8f624136b45311b092d79b2f44cf791190b390
Diego Pérez <diegodevelops@gmail.com>
2024-02-07 11:05:45 -0400

    Modified database json file and including it, plus adding NOT NULL constratints to migrations

--- migrations/sqls/20240131152700-orders-table-up.sql
+++ migrations/sqls/20240131152700-orders-table-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE orders (id SERIAL PRIMARY KEY, status VARCHAR(64), user_id bigint REFERENCES users(id));
+CREATE TABLE orders (id SERIAL PRIMARY KEY, status VARCHAR(64) NOT NULL, user_id bigint NOT NULL REFERENCES users(id));
--- migrations/sqls/20240201151954-order-products-table-up.sql
+++ migrations/sqls/20240201151954-order-products-table-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE order_products (id SERIAL PRIMARY KEY, quantity integer, order_id bigint REFERENCES orders(id), product_id bigint REFERENCES products(id));
+CREATE TABLE order_products (id SERIAL PRIMARY KEY, quantity integer NOT NULL, order_id bigint NOT NULL REFERENCES orders(id), product_id bigint NOT NULL REFERENCES products(id));
